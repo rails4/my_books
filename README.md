1574d7ed55f3fd1ace5aabed7bc8536f57286fde
Wlodek Bzyl <matwb@ug.edu.pl>
2014-02-24 16:42:17 +0100

    pierwszy import

---
+++ README.md
@@ -0,0 +1,19 @@
+## Moje książki
+
+Przykładowa aplikacja korzystająca z gemu
+*carrierwave* oraz biblioteki JavaScript
+Masonry/Isotope.
+
+
+## TODO
+
+* Ruby version: **2.1.0*, gemset: **my_books**
+* Rails version: **4.0.3**
+
+* System dependencies
+* Configuration
+* Database creation
+* Database initialization
+* How to run the test suite
+* Services (job queues, cache servers, search engines, etc.)
+* Deployment instructions
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyBooks::Application.config.secret_key_base = 'ac4b2ebf3dc7eb48ffa48799e26f3b9ef4fd40b81d168fe96b47d80bc76707409b0db0d610480c614c194de988e79519630505a6c13489c4e1b984d46067179a'
